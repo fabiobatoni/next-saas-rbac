e3ce9d864bd7bd65e49a523081b134bb00af234a
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2024-08-14 15:00:25 -0300

    api index

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,9 @@ import { ability } from '@saas/auth'
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+
+console.log(userCannotDeleteOtherUsers)
 
 console.log(userCanDeleteOtherUsers)
 console.log(userCanInviteSomeoneElse)
